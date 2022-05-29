ffb4f0859f20a6b8424ff38dfd66a5c3da8e7e1a
romchik <romchikae@gmail.com>
2022-05-29 15:37:50 +0300

    add Ln

---
+++ lab_2/src/main/java/Main.java
@@ -0,0 +1,8 @@
+import functions.logarithms.Ln;
+
+public class Main {
+    public static void main(String[] args) {
+        Ln test = new Ln(0.001);
+        System.out.println("ans: " + test.compute(30.0));
+    }
+}
--- lab_2/src/main/java/functions/logarithms/Ln.java
+++ lab_2/src/main/java/functions/logarithms/Ln.java
@@ -1,5 +1,47 @@
 package functions.logarithms;
 
-public class Ln {
+import functions.Func;
 
+import java.text.DecimalFormat;
+
+public class Ln extends Func {
+    private static final int ITERATION_NUMBER = 1000000;
+    // private final DecimalFormat df = new DecimalFormat("#.#####");
+
+    public Ln(double accuracy) {
+        super(accuracy);
+    }
+
+    @Override
+    public double compute(double x) {
+        if (Double.isNaN(x) || x < 0.0) {
+            return Double.NaN;
+        } else if (x == Double.POSITIVE_INFINITY) {
+            return Double.POSITIVE_INFINITY;
+        }
+
+        double currentValue = 0;
+        double previousValue;
+        int iteration = 1;
+
+        if (Math.abs(x - 1) <= 1) {
+            do {
+                previousValue = currentValue;
+                currentValue += ((Math.pow(-1, iteration - 1) * Math.pow(x - 1, iteration)) / iteration);
+                iteration++;
+            } while (getAccuracy() <= Math.abs(currentValue - previousValue) && iteration < ITERATION_NUMBER);
+        } else {
+            do {
+                previousValue = currentValue;
+                currentValue += ((Math.pow(-1, iteration - 1) * Math.pow(x - 1, -iteration)) / iteration);
+                iteration++;
+            } while (getAccuracy() <= Math.abs(currentValue - previousValue) && iteration < ITERATION_NUMBER);
+
+            currentValue += compute(x - 1);
+        }
+
+        DecimalFormat df = new DecimalFormat("#.#####");
+
+        return Double.parseDouble(df.format(currentValue).replace(",", "."));
+    }
 }
