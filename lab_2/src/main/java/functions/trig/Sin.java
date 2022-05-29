9c1752bcd517020d49a5ec10ae966699e04b1c07
romchik <romchikae@gmail.com>
2022-05-29 16:17:48 +0300

    add sin

--- lab_2/src/main/java/functions/trig/Sin.java
+++ lab_2/src/main/java/functions/trig/Sin.java
@@ -1,7 +1,31 @@
-//package functions.trig;
-//
-//import functions.Func;
-//
-//public class Sin extends Func {
-//
-//}
+package functions.trig;
+
+import functions.Func;
+
+public class Sin extends Func {
+    private final Cos cosFunction;
+
+    public Sin(double accuracy, Cos cosFunction) {
+        super(accuracy);
+        this.cosFunction = cosFunction;
+    }
+
+    @Override
+    public double compute(double x) {
+        int period = (int) (x / (Math.PI * 2));
+        x -= period * Math.PI *2;
+
+        int sign;
+        if(x > Math.PI && x < Math.PI * 2) {
+            sign = -1;
+        }
+        else if(x < 0.0 && x > -Math.PI) {
+            sign = -1;
+        }
+        else {
+            sign = 1;
+        }
+
+        return sign * Math.sqrt(1 - Math.pow(cosFunction.compute(x), 2));
+    }
+}
