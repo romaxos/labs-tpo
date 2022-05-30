39b61b54039fbea972d0fca896e3a355c064bb76
romchik <romchikae@gmail.com>
2022-05-30 08:16:08 +0300

    add all tests for log 2

--- lab_2/src/test/java/functions/logarithms/LogTwoTest.java
+++ lab_2/src/test/java/functions/logarithms/LogTwoTest.java
@@ -1,4 +1,54 @@
 package functions.logarithms;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class LogTwoTest {
+
+    private static final double DELTA = 0.05;
+    public static final double ACCURACY = 0.001;
+
+    private LogTwo logTwoFunction;
+
+    @BeforeEach
+    void setUp() {
+        this.logTwoFunction = new LogTwo(ACCURACY, LogFactory.getLnMock());
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/functions/logarithms/log_two_data_test.csv")
+    @DisplayName("General log two tests")
+    public void tableValuesTest(double numerator, double denominator, double expected) {
+        double actual = logTwoFunction.compute(numerator * Math.PI / denominator);
+        assertEquals(expected, actual, DELTA);
+    }
+
+    @Test
+    @DisplayName("Test negative infinity")
+    public void negativeInfinityStubsTest() {
+        double expected = Double.NaN;
+        double actual = logTwoFunction.compute(Double.NEGATIVE_INFINITY);
+        assertEquals(expected, actual, DELTA);
+    }
+
+    @Test
+    @DisplayName("Test positive infinity")
+    public void positiveInfinityStubsTest() {
+        double expected = Double.POSITIVE_INFINITY;
+        double actual = logTwoFunction.compute(Double.POSITIVE_INFINITY);
+        assertEquals(expected, actual, DELTA);
+    }
+
+    @Test
+    @DisplayName("Test nan")
+    public void nanStubsTest() {
+        double expected = Double.NaN;
+        double actual = logTwoFunction.compute(Double.NaN);
+        assertEquals(expected, actual, DELTA);
+    }
 }
