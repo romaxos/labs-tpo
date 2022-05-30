e47009e1aefdba19e6a80023381e3e1750390fda
romchik <romchikae@gmail.com>
2022-05-30 09:41:44 +0300

    add first integration test

--- lab_2/src/test/java/main/MyAppTest.java
+++ lab_2/src/test/java/main/MyAppTest.java
@@ -1,5 +1,10 @@
 package main;
 
+import functions.logarithms.LogFactory;
+import functions.trigonometry.TrigFactory;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 public class MyAppTest {
     private static final double DELTA = 0.05;
     private static final double ACCURACY = 0.001;
@@ -7,4 +12,11 @@ public class MyAppTest {
     static MyApp myApp;
 
 
+    @ParameterizedTest
+    @CsvFileSource(resources = "/main/integration_test.csv")
+    public void allStubsTest(double numerator, double denominator, double expected) {
+        myApp = new MyApp(ACCURACY, TrigFactory.getSinMock(), LogFactory.getLnMock(), LogFactory.getLogTwoBaseMock());
+        double actual = myApp.compute(numerator * Math.PI / denominator);
+        assertEquals(expected, actual, DELTA);
+    }
 }
