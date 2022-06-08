54ebf95a21a531cabe16641b48f71c6194e93541
romchik <romchikae@gmail.com>
2022-06-08 23:31:44 +0300

    add logger

--- lab_2/src/main/java/Main.java
+++ lab_2/src/main/java/Main.java
@@ -2,30 +2,16 @@ import functions.logarithms.Ln;
 import functions.logarithms.LogTwo;
 import functions.trigonometry.Cos;
 import functions.trigonometry.Sin;
+import logger.CsvLogger;
 import main.MyApp;
 
 public class Main {
-
-    private static final double ACCURACY = 0.001;
     public static void main(String[] args) {
-        Cos cosFunction = new Cos(ACCURACY);
-        Sin sinFunction = new Sin(ACCURACY, cosFunction);
-        Ln lnFunction = new Ln(ACCURACY);
-        LogTwo logTwoFunction = new LogTwo(ACCURACY, lnFunction);
-        MyApp myApp = new MyApp(ACCURACY, sinFunction, lnFunction, logTwoFunction);
-//        CsvLogger csvLogger = new CsvLogger("computer_result.csv");
-//        csvLogger.log(myApp);
-
-
-        System.out.println(sinFunction.compute(10 * Math.PI / 3.1415926));
-        System.out.println(sinFunction.compute(100 * Math.PI / 3.1415926));
-        System.out.println(sinFunction.compute(1000 * Math.PI / 3.1415926));
-        System.out.println(sinFunction.compute(-10 * Math.PI / 3.1415926));
-        System.out.println(sinFunction.compute(-100 * Math.PI / 3.1415926));
-        System.out.println(sinFunction.compute(-1000 * Math.PI / 3.1415926));
-
-
-
-
+        CsvLogger csvLogger = new CsvLogger();
+        csvLogger.goCsv("Sin", -2 * Math.PI, 2 * Math.PI, Math.PI/4);
+        csvLogger.goCsv("Cos", -2 * Math.PI, 2 * Math.PI, Math.PI/4);
+        csvLogger.goCsv("Ln", -2 * Math.PI, 2 * Math.PI, Math.PI/4);
+        csvLogger.goCsv("LogTwo", -2 * Math.PI, 2 * Math.PI, Math.PI/4);
+        csvLogger.goCsv("MyFunction", -2 * Math.PI, 2 * Math.PI, Math.PI/4);
     }
 }
--- lab_2/src/main/java/logger/CsvLogger.java
+++ lab_2/src/main/java/logger/CsvLogger.java
@@ -1,20 +1,18 @@
 package logger;
-
+import au.com.bytecode.opencsv.CSVWriter;
 import functions.logarithms.Ln;
 import functions.logarithms.LogTwo;
 import functions.trigonometry.Cos;
 import functions.trigonometry.Sin;
 import main.MyApp;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.PrintStream;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CsvLogger {
-    // private String filePath = "/home/grek/labs-tpo/lab_2/src/main/resources/";
     private static final double ACCURACY = 0.001;
     private final Sin sinFunction;
     private final Cos cosFunction;
@@ -31,11 +29,49 @@ public class CsvLogger {
     }
 
     public void goCsv(String funcName, double start, double end, double step) {
+        List<String[]> data = new ArrayList<>();
         switch (funcName) {
-            case "Sin" {
-
+            case "Sin" -> {
+                for (double i = start; i <= end; i += step) {
+                    data.add(new String[]{String.valueOf(i), String.valueOf(sinFunction.compute(i))});
+                }
+                write(data, "sin_res.csv");
+            }
+            case "Cos" -> {
+                for (double i = start; i <= end; i += step) {
+                    data.add(new String[]{String.valueOf(i), String.valueOf(cosFunction.compute(i))});
+                }
+                write(data, "cos_res.csv");
+            }
+            case "Ln" -> {
+                for (double i = start; i <= end; i += step) {
+                    data.add(new String[]{String.valueOf(i), String.valueOf(lnFunction.compute(i))});
+                }
+                write(data, "ln_res.csv");
+            }
+            case "LogTwo" -> {
+                for (double i = start; i <= end; i += step) {
+                    data.add(new String[]{String.valueOf(i), String.valueOf(logTwoFunction.compute(i))});
+                }
+                write(data, "log_two_res.csv");
+            }
+            case "MyFunction" -> {
+                for (double i = start; i <= end; i += step) {
+                    data.add(new String[]{String.valueOf(i), String.valueOf(myAppFunction.compute(i))});
+                }
+                write(data, "my_func_res.csv");
             }
         }
     }
 
+    private void write(List<String[]> data, String fileName) {
+        try {
+            String csv = "/home/grek/labs-tpo/lab_2/src/main/resources/" + fileName;
+            CSVWriter writer = new CSVWriter(new FileWriter(csv));
+            data.forEach(writer::writeNext);
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
